f8529a98d396921615351ab83d52a4514b170c5b
zezang <zezang@gmail.com>
2023-03-02 20:50:02 -0800

    Login and signup page working

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import {Route, Routes, Link, Navigate} from "react-router-dom";
 import axios from 'axios';
@@ -6,11 +6,18 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import NavBar from './components/NavBar';
 
+import './styles/main.css';
+
 const App = () => {
     const [loggedIn, setLoggedIn] = useState(false);
 
+    useEffect(() => {
+      const cookies = document.cookie.split(';');
+      if (cookies.includes('jwt')) setLoggedIn(true);
+    }, []);
+
     return (
-      <>
+      <div id='main-page'>
         <Routes>
           <Route path='/' element={<Login setLoggedIn={setLoggedIn}/>}/>
           {loggedIn && 
@@ -27,7 +34,7 @@ const App = () => {
             
           } 
         </Routes>     
-      </>
+      </div>
     );
 };
 
